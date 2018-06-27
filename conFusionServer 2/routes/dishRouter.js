aa4cf3134171703a073c4cdd139820c166e51bf9
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2018-06-27 16:54:08 +0530

    REST API with MONGO Part 1

---
+++ conFusionServer 2/routes/dishRouter.js
@@ -0,0 +1,125 @@
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const mongoose = require ('mongoose');
+
+//Importing the schema we created in the models folder
+const Dishes = require('../models/dishes');
+
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json());
+
+dishRouter.route('/')
+
+
+//Setting up all the endpoints for the /dishes
+.get((req,res,next) =>{
+	//Here we are expecting to get all the dishes, so we need to find all the dishes in DB
+	Dishes.find({})
+	.then((dishes) =>{
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(dishes);
+	},
+	(err) =>{
+		next(err);
+	})
+	.catch((err) =>{
+		next(err);
+	})
+})
+
+.post((req,res,next) =>{
+	//It will take the document to be posted from the body of the request
+	Dishes.create(req.body)
+	.then((dish) =>{
+		console.log('Dish Created ', dish);
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(dish);
+	},
+	(err) =>{
+		next(err);
+	})
+	.catch((err) =>{
+		next(err);
+	})	
+})
+
+.put((req,res,next) =>{
+	res.statusCode = 403;
+	res.end('PUT operation not supported on /dishes');
+})
+
+.delete((req,res,next) =>{
+	Dishes.remove({})
+	.then((resp) =>{
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(resp);
+	},
+	(err) =>{
+		next(err);
+	})
+	.catch((err) =>{
+		next(err);
+	})
+});
+
+
+
+//Setting up all the endpoints for the /dishes/:dishId
+
+dishRouter.route('/:dishId')
+
+.get((req,res,next) =>{
+	Dishes.findById(req.params.dishId)
+	.then((dish) =>{
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(dish);
+	},
+	(err) =>{
+		next(err);
+	})
+	.catch((err) =>{
+		next(err);
+	})	
+
+})
+
+.post((req,res,next) =>{
+	res.statusCode = 403;
+	res.end('POST operation not supported on /dishes/'+req.params.dishId);
+})
+
+.put((req,res,next) =>{
+	Dishes.findByIdAndUpdate(req.params.dishId,{
+		$set: req.body
+	}, {new: true})
+	.then((dish) =>{
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json(dish);
+	},
+	(err) =>{
+		next(err);
+	})
+	.catch((err) =>{
+		next(err);
+	})
+
+})
+
+.delete((req, res, next) => {
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+
+module.exports = dishRouter;
---
+++ conFusionServer 2/routes/leaderRouter.js
@@ -0,0 +1,60 @@
+const express = require ('express');
+const bodyParser = require ('body-parser');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+
+
+//Setting up all the endpoints for the /leaders
+.all((req, res, next) =>{
+	res.statusCode = 200;
+	res.setHeader('Content-Type','text/plain');
+	next();
+})
+
+.get((req,res,next) =>{
+	res.end("will send all the leaders to you!");
+})
+
+.post((req,res,next) =>{
+	res.end('Will add the leader: ' + req.body.name +' with details: ' + req.body.description);
+})
+
+.put((req,res,next) =>{
+	res.statusCode = 403;
+	res.end('PUT operation not supported on /leaders');
+})
+
+.delete((req,res,next) =>{
+	res.end("deleting all the leaders!");
+});
+
+
+
+//Setting up all the endpoints for the /leaders/:leaderId
+
+leaderRouter.route('/:leaderId')
+
+.get((req,res,next) =>{
+	res.end("will send details of the leader: " + req.params.leaderId + " to you!");
+})
+
+.post((req,res,next) =>{
+	res.statusCode = 403;
+	res.end('POST operation not supported on /leaders/'+req.params.leaderId);
+})
+
+.put((req,res,next) =>{
+	res.write('Updating the leader: '+req.params.leaderId + '\n');
+	res.end('will update the leader :'+req.body.name + ' with details '+ req.body.description);
+})
+
+.delete((req,res,next) =>{
+	res.end("deleting the leader: "+req.params.leaderId);
+});
+
+
+module.exports = leaderRouter;
