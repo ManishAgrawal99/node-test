4fd59182c28a300146d110d7616544f4888be7fd
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2018-08-06 12:36:22 +0530

    Facebook login implemented: Not working

---
+++ OAuth/config.js
@@ -0,0 +1,9 @@
+//Stores configuration info about our server
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId: '1639608519482783',
+        clientSecret: '1c54a9cc7815bc0e435c1a38eed77a3a'
+    }
+}
